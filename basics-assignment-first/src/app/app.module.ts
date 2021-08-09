36abb186d0a1bb8c7fdc14957d41d23af7582347
Filipe Silva <filipeqs@outlook.com>
2021-08-09 19:50:19 -0400

    first assignment

---
+++ basics-assignment-first/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { WarningAlert } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+
+@NgModule({
+    declarations: [AppComponent, WarningAlert, SuccessAlertComponent],
+    imports: [BrowserModule],
+    providers: [],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
