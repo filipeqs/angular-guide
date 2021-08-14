98be76118d966d6d4bf3bbe8f374795583db6aea
Filipe Silva <filipeqs@outlook.com>
2021-08-14 15:24:47 -0400

    data bindings

---
+++ data-bindings/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-cockpit',
+    templateUrl: './cockpit.component.html',
+    styleUrls: ['./cockpit.component.css'],
+})
+export class CockpitComponent implements OnInit {
+    newServerName = '';
+    newServerContent = '';
+
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    // onAddServer() {
+    //     this.serverElements.push(new Server('server', this.newServerName, this.newServerContent));
+    // }
+
+    // onAddBlueprint() {
+    //     this.serverElements.push({
+    //         type: 'blueprint',
+    //         name: this.newServerName,
+    //         content: this.newServerContent,
+    //     });
+    // }
+}
