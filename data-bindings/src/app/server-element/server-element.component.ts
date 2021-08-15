b60ec6f1389544286656dd384f4158e955fdcd74
Filipe Silva <filipeqs@outlook.com>
2021-08-15 11:11:49 -0400

    event binding

--- data-bindings/src/app/server-element/server-element.component.ts
+++ data-bindings/src/app/server-element/server-element.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+    Component,
+    ContentChild,
+    ElementRef,
+    Input,
+    OnInit,
+    ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
     selector: 'app-server-element',
@@ -7,6 +14,7 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
     @Input('srvElement') element: { type: string; name: string; content: string };
+    @ContentChild('contentParagraph', { static: false }) paragraph: ElementRef;
 
     constructor() {}
 
