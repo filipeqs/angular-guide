be2c45d2ba70cec6857840139b5fad06672ef68a
Filipe Silva <filipeqs@outlook.com>
2021-09-09 07:52:37 -0400

    can deactivate guard

--- routing/src/app/servers/edit-server/edit-server.component.ts
+++ routing/src/app/servers/edit-server/edit-server.component.ts
@@ -1,27 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { ServersService } from '../servers.service';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
 
 @Component({
     selector: 'app-edit-server',
     templateUrl: './edit-server.component.html',
     styleUrls: ['./edit-server.component.css'],
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
     server: { id: number; name: string; status: string };
     serverName = '';
     serverStatus = '';
     allowEdit = false;
+    changesSaved = false;
 
-    constructor(private serversService: ServersService, private route: ActivatedRoute) {}
+    constructor(
+        private serversService: ServersService,
+        private route: ActivatedRoute,
+        private router: Router,
+    ) {}
+    canDeactivate(): boolean | Observable<boolean> | Promise<boolean> {
+        if (!this.allowEdit) return true;
+
+        if (
+            (this.serverName !== this.server.name || this.serverStatus !== this.server.status) &&
+            !this.changesSaved
+        ) {
+            return confirm('Do you want to discard the changes?');
+        } else {
+            return true;
+        }
+    }
 
     ngOnInit() {
         this.route.queryParams.subscribe((queryParams: Params) => {
             this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
         });
         this.route.fragment.subscribe();
-        this.server = this.serversService.getServer(1);
+        const id = +this.route.snapshot.params['id'];
+        this.server = this.serversService.getServer(id);
         this.serverName = this.server.name;
         this.serverStatus = this.server.status;
     }
@@ -31,5 +51,7 @@ export class EditServerComponent implements OnInit {
             name: this.serverName,
             status: this.serverStatus,
         });
+        this.changesSaved = true;
+        this.router.navigate(['../'], { relativeTo: this.route });
     }
 }
